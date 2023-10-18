a731b585902b53dcda406a63f636f77fd33a53f4
Pedro Lucas Santos <85002280+PedroLuc-cpu@users.noreply.github.com>
2023-10-18 17:32:43 -0300

    reat: criado os metodos http de tarefa

--- ApI_task/ApI_task/Controllers/TarefaController.cs
+++ ApI_task/ApI_task/Controllers/TarefaController.cs
@@ -17,7 +17,7 @@ namespace ApI_task.Controllers
         }
 
         [HttpGet]
-        public ActionResult<List<TarefaModels>> BuscarTodasTarefas() 
+        public ActionResult<List<TarefaModels>> BuscarTodasTarefas()
         {
             return Ok();
         }
@@ -28,6 +28,28 @@ namespace ApI_task.Controllers
             TarefaModels tarefa = await _tarefaRepositorio.BurcarPorId(id);
             return Ok(tarefa);
 
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TarefaModels>> Cadastrar([FromBody] TarefaModels tarefaModel)
+        {
+            TarefaModels tarefa = await _tarefaRepositorio.Adicionar(tarefaModel);
+            return Ok(tarefa);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<TarefaModels>> Atualizar([FromBody] TarefaModels tarefaModel , int id)
+        {
+            tarefaModel.Id = id;
+            TarefaModels tarefa = await _tarefaRepositorio.Atualizar(tarefaModel , id);
+            return Ok(tarefa);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<TarefaModels>> Deletar(int id)
+        {
+            bool delatado = await _tarefaRepositorio.Deleta(id);
+            return Ok(delatado);
         }
     }
 }
